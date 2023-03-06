3f14f5365f79ffb01a27ece672be8c2d47ce11c5
Christian Guether <christian.guether@softwareag.com>
2023-03-06 13:14:26 +0100

    use constant for navigator node

--- part-04/my-c8y-application/src/named-dashboard/named-dashboard.factory.ts
+++ part-04/my-c8y-application/src/named-dashboard/named-dashboard.factory.ts
@@ -3,12 +3,14 @@ import { NavigatorNode, NavigatorNodeFactory } from '@c8y/ngx-components';
 
 @Injectable()
 export class NamedDashboardNavigationFactory implements NavigatorNodeFactory {
+  private readonly DASHBOARD_NAVIGATOR_NODE = new NavigatorNode({
+    label: 'Dashboard',
+    icon: 'robot',
+    path: 'named-dashboard',
+    priority: 50,
+  });
+
   get() {
-    return new NavigatorNode({
-      label: 'Dashboard',
-      icon: 'robot',
-      path: 'named-dashboard',
-      priority: 50,
-    });
+    return this.DASHBOARD_NAVIGATOR_NODE;
   }
 }
